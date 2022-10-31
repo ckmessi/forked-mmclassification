9abd354bca7afd3a7229974d759a764cbcb344cd
chenkai <ckmessi@163.com>
2022-10-31 14:57:20 +0800

    feat: add script to forward ood dataset

---
+++ scripts/20221031_generate_scores_result_for_ood_data.sh
@@ -0,0 +1,3 @@
+# 输出领域外数据集的推理结果
+python demo/image_forward_result_for_folder.py /data/chenkai/datasets/public/MNIST_plain/test/1 tmp/mnist/mnist-test-1.json configs/resnet/exp_20221009/20221009_resnet50_8xb16-mixup_cifar10.py work_dirs/20221009_resnet50_8xb16-mixup_cifar10/epoch_200.pth
+
