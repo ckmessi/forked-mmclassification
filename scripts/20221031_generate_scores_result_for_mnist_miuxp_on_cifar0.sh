bc07f4726d4a4a5c1c22f8fc5ee7654372ad81e9
chenkai <ckmessi@163.com>
2022-10-31 16:55:34 +0800

    feat: add scripts

---
+++ scripts/20221031_generate_scores_result_for_mnist_miuxp_on_cifar0.sh
@@ -0,0 +1,7 @@
+# 输出领域外数据集的推理结果
+for CLASS_INDEX in 0 1 2 3 4 5 6 7 8 9
+do
+    echo "forward for class $CLASS_INDEX"
+    python demo/image_mixup_forward_result_for_folder.py /data/chenkai/datasets/public/MNIST_plain/test/$CLASS_INDEX data/cifar10/test_split/0/1001_airplane.png tmp/mnist-test-mixup-0/mnist-$CLASS_INDEX-test-mixup-0.json configs/resnet/exp_20221009/20221009_resnet50_8xb16-mixup_cifar10.py work_dirs/20221009_resnet50_8xb16-mixup_cifar10/epoch_200.pth
+done
+
