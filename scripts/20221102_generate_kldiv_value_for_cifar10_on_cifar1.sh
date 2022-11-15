7322853d97674b0f23c443dfcba1a4ca25d5ce26
chenkai <ckmessi@163.com>
2022-11-15 14:01:42 +0800

    exp: add exp files

---
+++ scripts/20221102_generate_kldiv_value_for_cifar10_on_cifar1.sh
@@ -0,0 +1,7 @@
+# 输出领域外数据集的推理结果
+for CLASS_INDEX in 0 1 2 3 4 5 6 7 8 9
+do
+    echo "forward for class $CLASS_INDEX"
+    python demo/20221101_image_mixup_forward_result_for_folder_calculate_kldiv.py data/cifar10/test_split/$CLASS_INDEX data/cifar10/test_split/1/1005_automobile.png tmp/20221102_kldiv_cifar10/20221102_kldiv_cifar10_"$CLASS_INDEX"_target_1.json configs/resnet/exp_20221009/20221009_resnet50_8xb16-mixup_cifar10.py work_dirs/20221009_resnet50_8xb16-mixup_cifar10/epoch_200.pth
+done
+
